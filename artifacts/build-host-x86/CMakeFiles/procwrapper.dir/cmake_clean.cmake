bc640bf598427818acd1326d4be6dcd59f9aa59b
Mungert69 <contact@mahadeva.co.uk>
2025-08-26 19:00:55 +0100

    added arm libraries

---
+++ artifacts/build-host-x86/CMakeFiles/procwrapper.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/mahadeva/code/ProcWrapper/artifacts/install-host-x86/libprocwrapper.pdb"
+  "/home/mahadeva/code/ProcWrapper/artifacts/install-host-x86/libprocwrapper.so"
+  "CMakeFiles/procwrapper.dir/procwrapper.c.o"
+  "CMakeFiles/procwrapper.dir/procwrapper.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/procwrapper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
